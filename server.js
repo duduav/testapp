4662b1de62c1bfde789d9285d954c558b05b8e12
davinoam <dudu.avinoam@allscripts.com>
2020-07-28 15:20:46 +0300

    adding server js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.static('./dist/test-app'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'test-app'}
+  );
+  });
+app.listen(process.env.PORT || 8080);
